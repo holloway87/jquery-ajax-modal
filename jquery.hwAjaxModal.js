783c188538edd830540bd3bcc4e7cfcdb0fd1f52
Thomas Rudolph <me@holloway-web.de>
2018-07-11 16:59:24 +0200

    added factory wrapper for amd, node and browser globals

--- jquery.hwAjaxModal.js
+++ jquery.hwAjaxModal.js
@@ -1,4 +1,15 @@
-(function ($) {
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // Node/CommonJS
+        factory(require('jquery'));
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
     'use strict';
 
     /**
@@ -234,4 +245,4 @@
             }
         });
     };
-}(jQuery));
+}));
